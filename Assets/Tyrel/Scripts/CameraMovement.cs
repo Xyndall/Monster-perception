0cb03a9c6d50896b688ad3e20d5a42f8449f0130
tyrel <tyrel.ratima@gmail.com>
2021-10-19 15:19:02 +1300

    Character Controller
    Character controller
    camera movement
    keyCard??

---
+++ Assets/Tyrel/Scripts/CameraMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CameraMovement : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+
+    public Transform playerBody;
+    float rotationX = 0f;
+
+    public GameObject Player = null;
+    public GameObject fpsCam = null;
+
+    void Start()
+    {
+        Cursor.lockState =  CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
+
+
+    void Update()
+    {
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        rotationX -= mouseY;
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(rotationX, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(transform.position, transform.forward, out hit, 10))
+            {
+
+                if(hit.collider.tag == "KeyCard")
+                {
+                    
+                    
+                    GameObject keyCard = hit.collider.gameObject;
+
+                    Instantiate(keyCard,  transform.position + transform.forward * 1.5f - Vector3.up, transform.rotation, fpsCam.transform);
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+        }
+
+    }
+
+}
---
+++ Assets/Tyrel/Scripts/CharacterController.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterController : MonoBehaviour
+{
+
+    public Image staminaBar = null;
+    public CanvasGroup sliderGroup = null;
+    public GameObject fpsCamera = null;
+
+    public CharacterController controller;
+
+    public float speed = 8;
+    float runSpeed = 20;
+    float normalSpeed = 8;
+    public float gravity = -9.8f;
+
+    public float stamina = 5f;
+    public float maxStamina = 5f;
+    public float minStamina = 0f;
+    public float staminaDrain = 2;
+    public float staminaRegen = 2;
+    bool sprinting = false;
+    bool regenerated = true;
+
+    Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fpsCamera.GetComponentInChildren<Camera>().fieldOfView = 60;
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        gameObject.transform.position += move * speed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (regenerated == true)
+            {
+                sprinting = true;
+                stamina -= staminaDrain * Time.deltaTime;
+                UpdateStamina(1);
+                fpsCamera.GetComponentInChildren<Camera>().fieldOfView = 75;
+                speed = runSpeed;
+                if (stamina <= 0)
+                {
+
+                    regenerated = false;
+                    sprinting = false;
+                    sliderGroup.alpha = 0;
+                }
+            }
+        }
+        else
+        {
+            sprinting = false;
+        }
+
+        
+        
+        if (sprinting == false)
+        {
+            speed = normalSpeed;
+            if (stamina <= maxStamina)
+            {
+                
+                UpdateStamina(1);
+                stamina += staminaRegen * Time.deltaTime;
+
+                if (stamina >= maxStamina)
+                {
+                    regenerated = true;
+                    sliderGroup.alpha = 0;
+                    
+                }
+            }
+
+        }
+
+
+    }
+
+    //public void Sprinting()
+    //{
+    //    if (regenerated == true)
+    //    {
+    //        sprinting = true;
+    //        stamina -= staminaDrain * Time.deltaTime;
+    //        UpdateStamina(1);
+    //        fpsCamera.GetComponentInChildren<Camera>().fieldOfView = 75;
+    //        speed = runSpeed;
+    //        if (stamina <= 0)
+    //        {
+                
+    //            regenerated = false;
+    //            sprinting = false;
+    //            sliderGroup.alpha = 0;
+    //        }
+    //    }
+    //}
+
+    void UpdateStamina(int value)
+    {
+        staminaBar.fillAmount = stamina / maxStamina;
+
+        if (value == 0)
+        {
+            sliderGroup.alpha = 0;
+        }
+        else
+        {
+            sliderGroup.alpha = 1;
+        }
+
+    }
+
+
+    
+
+}
