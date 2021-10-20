6d3bc7df513c2e59bc2e1f926a087237fea040c5
kyle <studio11keyuniverse@gmail.com>
2021-10-21 12:53:19 +1300

    Added scripts folder

---
+++ Assets/Kyle/Script/lights.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lights : MonoBehaviour
+{
+    Light Lightcolor;
+
+    public bool monstermove = false;
+    bool lightswitch = false;
+
+    float timewhitelight = 20;
+    float timeredlight = 10;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       Lightcolor = gameObject.GetComponent<Light>();
+     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timewhitelight <= 0)
+        {
+            lightswitch = true;
+        }
+
+        if (timeredlight <= 0)
+        {
+            lightswitch = false;
+        }
+
+        if (lightswitch == false)
+        {
+            timewhitelight -= Time.deltaTime;
+            timeredlight = 10;
+            Lightcolor.color = new Color(1, 1, 1, 1);
+            Debug.Log("White light timer " + timewhitelight);
+            monstermove = false;
+        }
+
+        if(lightswitch == true)
+        {
+            timeredlight -= Time.deltaTime;
+            timewhitelight = 20;
+            Debug.Log(timeredlight + "Red Light");
+            Lightcolor.color = new Color(1, 0, 0, 1);
+            Debug.Log("Red light timer " + timeredlight);
+            monstermove = true;
+        }
+
+    }
+}
