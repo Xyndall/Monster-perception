bc2255eaea96a1a4585ab743673732cb0097239e
Phoenix <phoenix.millar.020@gmail.com>
2021-10-20 15:04:47 +1300

    Collision and Updated AI
    Enemy AI now has behavior switches between patrolling an area and chasing after the player. When it collides with the player it sends them to a Game Over Screen

---
+++ Assets/Phoenix/Scripts/GameOver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public bool EndGame = false;
+
+    public void EndofGame()
+    {
+        //EndGame is true launch the Game Over Screen
+        if (EndGame == false)
+        {
+            EndGame = true;
+            GameOverScreen();
+        }
+    }
+    //Send Player to the Next Scene
+    void GameOverScreen()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+}
---
+++ Assets/Phoenix/Scripts/SceneManagement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SceneManagement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
